9f1ac9f8130aa3fa29c123386124ffc401317e7d
Chaitra Bhat <79207846+Chaitra-Bhat383@users.noreply.github.com>
2023-04-21 14:15:10 +0530

    files updated

--- springmvc/src/main/java/com/example/springmvc/repositories/UserRepository.java
+++ springmvc/src/main/java/com/example/springmvc/repositories/UserRepository.java
@@ -8,10 +8,6 @@ import com.example.springmvc.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author aditi
- */
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
     User findByUserId(final String UserId);
